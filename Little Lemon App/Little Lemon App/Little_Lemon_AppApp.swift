93fe7bc0a279d9717d96aac0dafdda51c82374e1
Guy <guychenevert98@gmail.com>
2023-12-08 12:23:35 -0500

    Inital XCode project setup

---
+++ Little Lemon App/Little Lemon App/Little_Lemon_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_AppApp.swift
+//  Little Lemon App
+//
+//  Created by Guy Chenevert on 2023-12-08.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
